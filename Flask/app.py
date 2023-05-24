5266b43005707a82c963d21a4dc63e821db8bfc4
jasmeeetSingh <jasmeet.singh.1997@gmail.com>
2023-05-24 16:49:30 -0500

    Flask app commit

--- Flask/app.py
+++ Flask/app.py
@@ -10,32 +10,19 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/process_text', methods=['POST'])
+@app.route('/set_schema', methods=['POST'])
 def process_text():
-    user_text = request.form['user_text']
-    processed_text = process_user_text(user_text)
-    processed_transcript = process_user_transcript(user_text)
-    return render_template('results.html', processed_text=processed_text, table_html=processed_transcript)
-
-def process_user_text(user_text):
-    
-    from transformers import PegasusForConditionalGeneration, PegasusTokenizer, SummarizationPipeline
-
-    model_name = "renegarza/PegasusMedicalSummary"
-    tokenizer = PegasusTokenizer.from_pretrained(model_name)
-    model = PegasusForConditionalGeneration.from_pretrained(model_name)
-
-    summarizer = SummarizationPipeline(model=model, tokenizer=tokenizer, device = 0)
-
-    summary = summarizer(user_text)
-    return summary[0]['summary_text']
-
-def process_user_transcript(user_text):
+    schema = request.form['user_text'].strip()
+    query = request.form['user_text1'].strip()
         
-    df = get_table(user_text)
-    table_html = df.to_html(index = False, justify = 'left')
+    processed_text = ''
+    try:    
+        tqlRunner = TQLRunner(schema)
+        processed_text = tqlRunner.get_SQL_query(query)
+    except:
+        return render_template('failure.html', actual_text = query)
     
-    return table_html
+    return render_template('results.html', processed_text=processed_text, actual_text = query)
 
 if __name__ == '__main__':
     app.run(debug=True)
