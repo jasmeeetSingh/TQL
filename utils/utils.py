e390efe9289b084676760a839b6a56176e917793
jasmeeetSingh <jasmeet.singh.1997@gmail.com>
2023-10-29 17:52:03 +0000

    Integration 3

--- utils/utils.py
+++ utils/utils.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 def get_spider_schema_table_files():
     
-    spider_query = pd.read_csv("spiderQueryData.csv")
-    spider_schema = pd.read_csv("tablesSchemaSpider.csv")
+    spider_query = pd.read_csv("gs://data_tql/spider/processed/spiderQueryData.csv")
+    spider_schema = pd.read_csv("gs://data_tql/spider/processed/Schemas/tablesSchemaSpider.csv")
     
     return spider_query, spider_schema
 
